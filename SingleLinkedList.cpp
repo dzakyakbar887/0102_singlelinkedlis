e29b1359fa98bbbaa692a9591e1245e7a868130b
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-04-23 12:25:19 +0700

    void addnode

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -15,3 +15,46 @@ class linkedList
 
     public:
     linkedList()
+    {
+        start = NULL;
+    }
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan nomor mahasiswa: ";
+        cin >> nim;
+
+        Node *nodebaru = new Node;
+        nodebaru->noMhs = nim;
+
+        if (start == NULL || nim <= start->noMhs)
+        {
+            if (start != NULL && nim == start->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+
+            nodebaru->next = start;
+            start = nodebaru;
+            return;
+        }
+
+            Node *previous = start;
+            Node *current = start;
+
+            while (current != NULL && nim >= current->noMhs)
+            {
+                if (nim == current->noMhs)
+                {
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                    return;
+
+                }
+                    previous = current;
+                    current = current->next;
+            }
+
+            nodebaru->next = current;
+            previous->next = nodebaru;
+    }
